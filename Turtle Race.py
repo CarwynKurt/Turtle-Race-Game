ed3b926b04921801f952cb7620a24cf3ae49cd76
CarwynKurt <carwynkurtarimado@gmail.com>
2022-11-19 21:17:20 +0800

    Design Soil

--- Turtle Race.py
+++ Turtle Race.py
@@ -19,6 +19,14 @@ tr.write("TURTLE RACE", font=("Times New Roman", 20, "bold"))
 tr.penup()
 
 # 3. Design Soil
+tr.goto(-400, -279)
+tr. color("chocolate")
+tr.begin_fill()
+tr.pendown()
+tr.forward(700)
+tr.right(90)
+tr.forward(700)
+tr.end_fill()
 # 4. Design Finish Line
 # 5. Input Racer 1
 # 6. Input Racer 2
