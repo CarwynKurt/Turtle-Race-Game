641470ccae4176751a2c60caa764efe0745cece2
CarwynKurt <carwynkurtarimado@gmail.com>
2022-11-19 20:55:04 +0800

    Import

---
+++ Turtle Race.py
@@ -0,0 +1,22 @@
+# Turtle Race
+# 1. Import Turtle and Random
+import turtle
+import time
+
+from turtle import Turtle
+from random import randint
+
+# 2. Design Grass
+# 3. Design Soil
+# 4. Design Finish Line
+# 5. Input Racer 1
+# 6. Input Racer 2
+# 7. Input Racer 3
+# 8. Input Racer 4
+# 9. Move the Turtle
+# 10 Setup Winner
+# 11 Monitor Distance
+# 12 Display the winner
+
+
+turtle.done()
